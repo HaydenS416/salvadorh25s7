bb56f7f41a2f09741bb0c72c4275b4d7260b7c80
HaydenS416 <164280188+HaydenS416@users.noreply.github.com>
2025-07-11 15:00:47 -0400

    added codes

--- assignment4/pt1/main.js
+++ assignment4/pt1/main.js
@@ -1,1 +1,19 @@
-console.log('pt1')
+console.log('pt1');
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+let storyText = 'It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day.'
+;
+let insertx = [
+    'Willy the Goblin',
+    'Big Daddy',
+    'Father Christmas'
+];
+
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+

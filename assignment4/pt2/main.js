965e7f15a0531ee292ea4abcfb85beaf0a033c58
HaydenS416 <164280188+HaydenS416@users.noreply.github.com>
2025-08-01 15:25:43 -0400

    Update main.js

--- assignment4/pt2/main.js
+++ assignment4/pt2/main.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
-
+const altText = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white flowers',
+  'pic4.jpg': 'Ancient Egyptian mural',
+  'pic5.jpg': 'A beautiful butterfly on a leaf'
+}
 /* Looping through images */
 
 const newImage = document.createElement('img');
